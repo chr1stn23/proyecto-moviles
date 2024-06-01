eb7b8d8bf042109f278176a848bd1337dbefbae1
Yannick <yannfscz@gmail.com>
2024-06-01 00:48:56 -0500

    feat (dependecies): add lombok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.volley)
     implementation(libs.gson)
+    implementation(libs.lombok)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
