5ea44ca3edf82efea17c6e33f6616b0e4a228e2f
Yannick <yannfscz@gmail.com>
2024-06-01 01:37:38 -0500

    fix (data): fix User

--- app/src/main/java/com/grupo8/proyecto/data/User.java
+++ app/src/main/java/com/grupo8/proyecto/data/User.java
@@ -1,7 +1,19 @@
 package com.grupo8.proyecto.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
+    @EqualsAndHashCode.Include
     private int id;
     private String nombre;
     private String email;
@@ -9,63 +21,4 @@ public class User {
     private String tipoUsuario;
     private String fotoPerfil;
 
-    public User() {
-    }
-
-    public User(int id, String nombre, String email, String fechaRegistro, String tipoUsuario, String fotoPerfil) {
-        this.id = id;
-        this.nombre = nombre;
-        this.email = email;
-        this.fechaRegistro = fechaRegistro;
-        this.tipoUsuario = tipoUsuario;
-        this.fotoPerfil = fotoPerfil;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFechaRegistro() {
-        return fechaRegistro;
-    }
-
-    public void setFechaRegistro(String fechaRegistro) {
-        this.fechaRegistro = fechaRegistro;
-    }
-
-    public String getTipoUsuario() {
-        return tipoUsuario;
-    }
-
-    public void setTipoUsuario(String tipoUsuario) {
-        this.tipoUsuario = tipoUsuario;
-    }
-
-    public String getFotoPerfil() {
-        return fotoPerfil;
-    }
-
-    public void setFotoPerfil(String fotoPerfil) {
-        this.fotoPerfil = fotoPerfil;
-    }
 }
